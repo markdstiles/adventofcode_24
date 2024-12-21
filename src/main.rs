d9a169dcc1ada246c638788c45a0a356ed954a50
markdstiles <119763563+markdstiles@users.noreply.github.com>
2024-12-21 23:40:10 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 pub mod day_1;
 pub mod day_2;
 pub mod day_3;
-pub mod day_4;
 
 use colored::Colorize;
 
 fn main() -> anyhow::Result<()> {
     
-    if let Ok(answer) = day_4::do_part1() {
+    if let Ok(answer) = day_3::do_part1() {
         println!("{} {}", "Part 1 answer:".green(), answer.to_string().blue().bold());
     }
 
-    if let Ok(answer) = day_4::do_part2() {
+    if let Ok(answer) = day_3::do_part2() {
         println!("{} {}", "Part 2 answer:".green(), answer.to_string().blue().bold());
     }
 
