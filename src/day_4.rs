95ba4f5ef88b7a4145011b585aedc94a86e5a3a5
markdstiles <mark.d.stiles@gmail.com>
2024-12-22 10:50:09 +0000

    Removed the disgusting use of iterators

--- src/day_4.rs
+++ src/day_4.rs
@@ -13,10 +13,10 @@ pub fn do_part1() -> anyhow::Result<i64> {
     let file = File::open(input_file.clone())?;
     let reader = BufReader::new(file);
 
-    let mut word_search: Vec<String> = Vec::new();
+    let mut word_search: Vec<Vec<char>> = Vec::new();
 
     for line in reader.lines() {
-        word_search.push(line?.clone());
+        word_search.push(line?.chars().collect());
     }
 
     // Word              S  S  S
@@ -34,31 +34,34 @@ pub fn do_part1() -> anyhow::Result<i64> {
     //Start at the top-left, if we find X then we scan in all directions for 'XMAS'
     let mut y: usize = 0;
     while y < height {
+        
         let mut x: usize = 0;
         while x < width {
 
-            if word_search[y].chars().nth(x).unwrap() == 'X' {
+            if word_search[y][x] == 'X' {
 
                 if x > 2 {
                     //Scan west
-                    if word_search[y][x-3..x] == *"SAM" {
+                    if word_search[y][x-3] == 'S'
+                    && word_search[y][x-2] == 'A'
+                    && word_search[y][x-1] == 'M' {
                         word_count += 1;
                     }
 
                     if y > 2 {
                         //Scan diagonally north-west
-                        if word_search[y-1].chars().nth(x-1).unwrap() == 'M'
-                        && word_search[y-2].chars().nth(x-2).unwrap() == 'A'
-                        && word_search[y-3].chars().nth(x-3).unwrap() == 'S' {
+                        if word_search[y-1][x-1] == 'M'
+                        && word_search[y-2][x-2] == 'A'
+                        && word_search[y-3][x-3] == 'S' {
                             word_count += 1;
                         }
                     }
 
                     if y < height - 3 {
                         //Scan diagonally south-west
-                        if word_search[y+1].chars().nth(x-1).unwrap() == 'M'
-                        && word_search[y+2].chars().nth(x-2).unwrap() == 'A'
-                        && word_search[y+3].chars().nth(x-3).unwrap() == 'S' {
+                        if word_search[y+1][x-1] == 'M'
+                        && word_search[y+2][x-2] == 'A'
+                        && word_search[y+3][x-3] == 'S' {
                             word_count += 1;
                         }
                     }
@@ -66,24 +69,26 @@ pub fn do_part1() -> anyhow::Result<i64> {
 
                 if x < width - 3 {
                     //Scan east
-                    if word_search[y][x..x+4] == *"XMAS" {
+                    if word_search[y][x+1] == 'M'
+                    && word_search[y][x+2] == 'A'
+                    && word_search[y][x+3] == 'S' {
                         word_count += 1;
                     }
 
                     if y > 2 { 
                         //Scan diagonally north-east
-                        if word_search[y-1].chars().nth(x+1).unwrap() == 'M'
-                        && word_search[y-2].chars().nth(x+2).unwrap() == 'A'
-                        && word_search[y-3].chars().nth(x+3).unwrap() == 'S' {
+                        if word_search[y-1][x+1] == 'M'
+                        && word_search[y-2][x+2] == 'A'
+                        && word_search[y-3][x+3] == 'S' {
                             word_count += 1;
                         }
                     }
 
                     if y < height - 3 { 
                         //Scan diagonally south-east
-                        if word_search[y+1].chars().nth(x+1).unwrap() == 'M'
-                        && word_search[y+2].chars().nth(x+2).unwrap() == 'A'
-                        && word_search[y+3].chars().nth(x+3).unwrap() == 'S' {
+                        if word_search[y+1][x+1] == 'M'
+                        && word_search[y+2][x+2] == 'A'
+                        && word_search[y+3][x+3] == 'S' {
                             word_count += 1;
                         }
                     }
@@ -91,18 +96,18 @@ pub fn do_part1() -> anyhow::Result<i64> {
 
                 if y > 2 {
                     //Scan north
-                    if word_search[y-1].chars().nth(x).unwrap() == 'M'
-                    && word_search[y-2].chars().nth(x).unwrap() == 'A'
-                    && word_search[y-3].chars().nth(x).unwrap() == 'S' {
+                    if word_search[y-1][x] == 'M'
+                    && word_search[y-2][x] == 'A'
+                    && word_search[y-3][x] == 'S' {
                         word_count += 1;
                     }
                 }
 
                 if y < height - 3 {
                     //Scan south
-                    if word_search[y+1].chars().nth(x).unwrap() == 'M'
-                    && word_search[y+2].chars().nth(x).unwrap() == 'A'
-                    && word_search[y+3].chars().nth(x).unwrap() == 'S' {
+                    if word_search[y+1][x] == 'M'
+                    && word_search[y+2][x] == 'A'
+                    && word_search[y+3][x] == 'S' {
                         word_count += 1;
                     }
                 }
@@ -148,15 +153,20 @@ pub fn do_part2() -> anyhow::Result<i64> {
     let mut y: usize = 1;
     while y < height-1 {
         let mut x: usize = 1;
+        
+        let center:Vec<char> = word_search[y].chars().collect();
+        let north: Vec<char> = word_search[y-1].chars().collect();
+        let south: Vec<char> = word_search[y+1].chars().collect();
+
         while x < width-1 {
-            mas_count += if word_search[y].chars().nth(x).unwrap() == 'A'                                                    //Center is A
-                        && (word_search[y-1].chars().nth(x+1).unwrap() == 'S' || word_search[y-1].chars().nth(x+1).unwrap() == 'M') //NE is either S or M
-                        && (word_search[y-1].chars().nth(x-1).unwrap() == 'S' || word_search[y-1].chars().nth(x-1).unwrap() == 'M') //NW
-                        && (word_search[y+1].chars().nth(x+1).unwrap() == 'S' || word_search[y+1].chars().nth(x+1).unwrap() == 'M') //SE
-                        && (word_search[y+1].chars().nth(x-1).unwrap() == 'S' || word_search[y+1].chars().nth(x-1).unwrap() == 'M') //SW
-                        && word_search[y-1].chars().nth(x+1).unwrap() != word_search[y+1].chars().nth(x-1).unwrap() //NE != SW (diagonals must not be the same, one is M and the other must be S)
-                        && word_search[y-1].chars().nth(x-1).unwrap() != word_search[y+1].chars().nth(x+1).unwrap() //NW != SE
-                        { 1 } else { 0 };   //Add 1 to count if we get a match
+            mas_count += if center[x] == 'A'                        //Center is A
+                        && (north[x+1] == 'S' || north[x+1] == 'M') //NE is either S or M
+                        && (north[x-1] == 'S' || north[x-1] == 'M') //NW
+                        && (south[x+1] == 'S' || south[x+1] == 'M') //SE
+                        && (south[x-1] == 'S' || south[x-1] == 'M') //SW
+                        && north[x+1] != south[x-1]                 //NE != SW (diagonals must not be the same, one is M and the other must be S)
+                        && north[x-1] != south[x+1]                 //NW != SE
+                        { 1 } else { 0 };                           //Add 1 to count if we get a match
             x += 1;
         }
         y += 1;
