f237f722d297361f2989e33649d40631ed8cfcac
markdstiles <mark.d.stiles@gmail.com>
2024-12-22 10:18:45 +0000

    Day4 - Part 2

--- src/day_4.rs
+++ src/day_4.rs
@@ -133,5 +133,34 @@ pub fn do_part2() -> anyhow::Result<i64> {
         word_search.push(line?.clone());
     }
 
-    Ok(0)
+    // Word              
+    // Search            
+    //                         S M  M M
+    //Searching for Cross-MAS - A    A  
+    //                         S M  S S
+
+    let height: usize = word_search.len();
+    let width: usize = word_search[0].len();
+    let mut mas_count = 0;
+
+    //Start at the top-left, we can skip the first row/col
+    //Look for the central A letter, then scan NE,NW,SE,SW for S or M
+    let mut y: usize = 1;
+    while y < height-1 {
+        let mut x: usize = 1;
+        while x < width-1 {
+            mas_count += if word_search[y].chars().nth(x).unwrap() == 'A'                                                    //Center is A
+                        && (word_search[y-1].chars().nth(x+1).unwrap() == 'S' || word_search[y-1].chars().nth(x+1).unwrap() == 'M') //NE is either S or M
+                        && (word_search[y-1].chars().nth(x-1).unwrap() == 'S' || word_search[y-1].chars().nth(x-1).unwrap() == 'M') //NW
+                        && (word_search[y+1].chars().nth(x+1).unwrap() == 'S' || word_search[y+1].chars().nth(x+1).unwrap() == 'M') //SE
+                        && (word_search[y+1].chars().nth(x-1).unwrap() == 'S' || word_search[y+1].chars().nth(x-1).unwrap() == 'M') //SW
+                        && word_search[y-1].chars().nth(x+1).unwrap() != word_search[y+1].chars().nth(x-1).unwrap() //NE != SW (diagonals must not be the same, one is M and the other must be S)
+                        && word_search[y-1].chars().nth(x-1).unwrap() != word_search[y+1].chars().nth(x+1).unwrap() //NW != SE
+                        { 1 } else { 0 };   //Add 1 to count if we get a match
+            x += 1;
+        }
+        y += 1;
+    }
+
+    Ok(mas_count)
 }
