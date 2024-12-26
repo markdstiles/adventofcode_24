063c5fec1e0bc1603ed1eaea0a509fe7ae260277
markdstiles <mark.d.stiles@gmail.com>
2024-12-26 22:21:22 +0000

    Update to main()

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@ use colored::Colorize;
 
 fn main() -> anyhow::Result<()> {
     
-    /*
     if let Ok(answer) = day_7::do_part1() {
         println!("{} {}", "Part 1 answer:".green(), answer.to_string().blue().bold());
     }
-    */
 
     if let Ok(answer) = day_7::do_part2() {
         println!("{} {}", "Part 2 answer:".green(), answer.to_string().blue().bold());
