69a39493d9aee8a96bd3724662df8562b27ab676
markdstiles <mark.d.stiles@gmail.com>
2025-01-11 23:19:25 +0000

    Day 16 - Part 2

--- src/day_16.rs
+++ src/day_16.rs
@@ -22,7 +22,7 @@ impl Node {
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 struct Edge {
     from: usize,
     to: usize,
@@ -37,11 +37,12 @@ impl Edge {
     }
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 struct State {
     cost: usize,
     node_id: usize,
     prev_node_id: Option<usize>,
+    path: Vec<Edge>,
 }
 
 // The priority queue depends on `Ord`.
@@ -70,7 +71,7 @@ fn calc_cost(from: Point<usize>, to: Point<usize>, current_direction: Point<i32>
     let p2: Point<i32> = to.into();
     let diff = p2 - p1;
 
-    //Calculate Manhattan cost, movement diagonally is not allowed or catered for
+    //Calculate Manhattan cost, diagonal movement is not allowed
     let mut cost = (diff.x.abs() + diff.y.abs()) as usize;
 
     if current_direction != get_direction(p1, p2) {
@@ -154,7 +155,7 @@ fn find_nodes(map: &mut [Vec<i32>], nodes: &mut Vec<Node>) -> (usize, usize) {
     //A set of the unvisited nodes - any nodes left unvisited can be marked as invalid
     let mut not_visited: HashSet<usize> = nodes.iter().map(|n| n.id).collect();
 
-    //Connect nodes together, starting with the start node
+    //Connect nodes together, starting with the start node, identify all edges
     let mut nodes_to_check: Vec<usize> = vec![start];
     while let Some(node_id) = nodes_to_check.pop() {
         if !not_visited.contains(&node_id) {
@@ -183,11 +184,13 @@ fn find_nodes(map: &mut [Vec<i32>], nodes: &mut Vec<Node>) -> (usize, usize) {
         not_visited.remove(&node_id);
     }
 
-    //Nodes with only 1 edge are dead ends, mark as invalid 
+    //Remove edges that don't help us find the goal - Maze's have dead ends
+
+    //Nodes with only 1 edge are dead ends, mark as invalid (exluding the starting node)
     //and remove any edges from other nodes that point to the invalid node
     // - repeat and until all nodes have at least 1 edge pointing to a valid node
     for node_id in 0..nodes.len() {
-        if nodes[node_id].edges.len() == 1 && node_id != end {
+        if nodes[node_id].edges.len() == 1 && !(node_id == end || node_id == start) {
             nodes[node_id].is_valid = false;
             let mut node_to_inspect = nodes[node_id].edges[0].to;
             let mut prev_node = node_id;
@@ -217,25 +220,40 @@ fn find_nodes(map: &mut [Vec<i32>], nodes: &mut Vec<Node>) -> (usize, usize) {
     (start, end)
 }
 
+struct ShortestPathResult {
+    shortest_path_cost: usize,
+    shortest_path: Vec<Edge>,
+}
 // Dijkstra's shortest path algorithm
 
 // Start at `start` and use `dist` to track the current shortest distance
 // to each node. This implementation isn't memory-efficient as it may leave duplicate
 // nodes in the queue. It also uses `usize::MAX` as a sentinel value,
 // for a simpler implementation.
-fn shortest_path(adj_list: &[Node], start: usize, goal: usize, start_direction: Point<i32>) -> Option<usize> {
+fn shortest_path(adj_list: &[Node], start: usize, goal: usize, start_direction: Point<i32>) -> Option<ShortestPathResult> {
     // dist[node] = current shortest distance from `start` to `node`
     let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
     let mut heap = BinaryHeap::new();
 
     // We're at `start`, with a zero cost
     dist[start] = 0;
-    heap.push(State { cost: 0, node_id: start, prev_node_id: None });
+    heap.push(State { cost: 0, node_id: start, prev_node_id: None, path: Vec::new() });
 
     // Examine the frontier with lower cost nodes first (min-heap)
-    while let Some(State { cost, node_id, prev_node_id }) = heap.pop() {
-        // Alternatively we could have continued to find all shortest paths
-        if node_id == goal { return Some(cost); }
+    while let Some(State { cost, node_id, prev_node_id, path }) = heap.pop() {
+        if node_id == goal {
+            //Need to include the goal edge
+            let mut path = path;
+
+            if let Some(prev_node_id) = prev_node_id {
+                path.push(Edge { from: prev_node_id, to: node_id });
+            }
+
+            return Some(ShortestPathResult {
+                shortest_path_cost: cost,
+                shortest_path: path,
+            })
+        }
 
         // Important as we may have already found a better way
         if cost > dist[node_id] || !adj_list[node_id].is_valid { continue; }
@@ -243,25 +261,33 @@ fn shortest_path(adj_list: &[Node], start: usize, goal: usize, start_direction: 
         // For each node we can reach, see if we can find a way with
         // a lower cost going through this node
         for edge in &adj_list[node_id].edges {
+            let mut previous_path = path.clone();
+
             let current_direction = if let Some(prev_node_id) = prev_node_id {
+                previous_path.push(Edge { from: prev_node_id, to: node_id });
                 get_direction(adj_list[prev_node_id].position.into(), adj_list[node_id].position.into())
             } else {
                 start_direction
             };
 
             let edge_cost = calc_cost(adj_list[node_id].position, adj_list[edge.to].position, current_direction);
-            let next = State { cost: cost + edge_cost, node_id: edge.to, prev_node_id: Some(node_id) };
+            
+            let next = State { 
+                cost: cost + edge_cost, 
+                node_id: edge.to, 
+                prev_node_id: Some(node_id),
+                path: previous_path, 
+            };
 
             // If so, add it to the frontier and continue
             if next.cost < dist[next.node_id] {
-                heap.push(next);
                 // Relaxation, we have now found a better way
                 dist[next.node_id] = next.cost;
+                heap.push(next);
             }
         }
     }
-
-    // Goal not reachable
+    
     None
 }
 
@@ -296,11 +322,54 @@ pub fn do_part1() -> anyhow::Result<i64> {
     //Start facing East
     let start_direction = Point::new(1, 0);
 
-    if let Some(shortest_path_cost) = shortest_path(&nodes, start, end, start_direction) {
+    if let Some(ShortestPathResult {shortest_path_cost, ..}) = shortest_path(&nodes, start, end, start_direction) {
         Ok(shortest_path_cost as i64)
     } else {
         Ok(0)
     }
+}
+
+fn find_optimal_paths(nodes: &[Node], start: usize, end: usize, start_direction: Point<i32>) -> HashSet<Edge> {
+    let mut path_nodes: HashSet<Edge> = HashSet::new();
+
+    //Get the optimal path and walk through it an edge at a time
+    //While the edge.to node is not the goal...
+    //If the edge.to node has multiple choices excluding the node it has come from
+    //Try to see if there is a route to the goal with the same cost as the optimal route
+
+    let mut prev_node_id: Option<usize> = None;
+    if let Some(ShortestPathResult {shortest_path_cost, shortest_path: path}) = shortest_path(nodes, start, end, start_direction) {
+        for edge in path {
+            path_nodes.insert(edge);
+
+            let current_node_id: usize = edge.from;
+            if let Some(prev_node_id) = prev_node_id {
+                //if we're at a junction
+                let other_edges = nodes[current_node_id].edges.iter().filter(|e| e.to != prev_node_id);
+
+                if other_edges.clone().count() > 1 {
+                    for edge in other_edges {
+                        if let Some(ShortestPathResult { shortest_path_cost: cost_from_start, .. }) = shortest_path(nodes, start, edge.to, start_direction) {
+                            let p1: Point<i32> = nodes[current_node_id].position.into();
+                            let p2: Point<i32> = nodes[edge.to].position.into();
+                            let new_direction = get_direction(p1, p2);
+                            if let Some(ShortestPathResult { shortest_path_cost: cost_from_node, shortest_path: new_path }) = shortest_path(nodes, edge.to, end, new_direction) {
+                                let total_cost = cost_from_start + cost_from_node;
+                                if total_cost <= shortest_path_cost {
+                                    new_path.iter().copied().for_each(|e| { path_nodes.insert(e); });
+                                    path_nodes.insert(Edge {from: current_node_id, to: edge.to });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            prev_node_id = Some(current_node_id);
+        }
+    }
+
+    path_nodes
 }
 
 pub fn do_part2() -> anyhow::Result<i64> {
@@ -313,19 +382,69 @@ pub fn do_part2() -> anyhow::Result<i64> {
 
     let file = File::open(input_file.clone())?;
     let reader = BufReader::new(file);
-    let mut map: Vec<Vec<u8>> = vec![];
+    let mut map: Vec<Vec<i32>> = vec![];
 
     for line in reader.lines() {
         let line = line?;
 
-        let row: Vec<u8> = line.chars().map(|c| match c {
-            '#' => 1,
-            'S' => 2,
-            'E' => 3,
-            _ => 0,
+        let row: Vec<i32> = line.chars().map(|c| match c {
+            '#' => -2,  //Wall
+            'S' => -3,  //Start location
+            'E' => -4,  //End location
+            _ => -1,    //Empty space
         }).collect();
         map.push(row);
     }
 
-    Ok(0)
+    let mut nodes: Vec<Node> = vec![];
+
+    let (start, end) = find_nodes(&mut map, &mut nodes);
+
+    //Start facing East
+    let start_direction = Point::new(1, 0);
+    let distinct_paths = find_optimal_paths(&nodes, start, end, start_direction);
+
+    //Plot paths on map and count the path tiles - keeping it simplez ;-)
+    for edge in distinct_paths {
+        let p1: Point<i32> = nodes[edge.from].position.into();
+        let p2: Point<i32> = nodes[edge.to].position.into();
+        let direction = get_direction(p1, p2);
+        let mut pos = p1;
+        
+        loop {
+            map[pos.y as usize][pos.x as usize] = -5;
+            if pos == p2 {
+                break;
+            }
+            pos += direction;
+        }
+    }
+
+    let tile_count = map.iter().flatten().filter(|&i| *i == -5).count();
+
+    /*
+    //Debug display of map
+    use colored::Colorize;
+
+    map.iter().for_each(|row| { 
+        row.iter().for_each(|col| {
+            print!("{}", match col {
+                -2 => "##".into(),
+                -3 => "SS".into(),
+                -4 => "EE".into(),
+                -5 => "O ".into(),
+                -1 => ". ".into(),
+                _ => { 
+                    if nodes[*col as usize].is_valid {
+                        format!("{:>2}", col)
+                    } else {
+                        format!("{:>2}", col.to_string().red())
+                    }
+                }
+            });
+        });
+        println!();
+    });
+    */
+    Ok(tile_count as i64)
 }
